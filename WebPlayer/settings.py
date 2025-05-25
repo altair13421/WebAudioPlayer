bdf929299868fcf7212da3a13ad7b50188467c56
Faez Akhtar <74133525+altair13421@users.noreply.github.com>
2025-05-25 15:51:15 +0500

    Update settings.py To include cors

--- WebPlayer/settings.py
+++ WebPlayer/settings.py
@@ -28,7 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+CORS_ALLOWED_ORIGINS = [
+    *
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'player.apps.PlayerConfig',
     'ImageFetcher.apps.ImageFetcherConfig',
     'django_bootstrap5',
@@ -48,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
